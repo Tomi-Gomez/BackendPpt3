18de0af5587d37de6ed779f78be343288da91fee
Tomi-Gomez <tomasngomez1@gmail.com>
2025-10-24 01:10:07 -0300

    fix: reporte medico v.2.1

--- src/main/java/com/proyecto_final_ppt3/Model/DTO/ReporteMedicoDTO.java
+++ src/main/java/com/proyecto_final_ppt3/Model/DTO/ReporteMedicoDTO.java
@@ -3,8 +3,7 @@ package com.proyecto_final_ppt3.Model.DTO;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
+
 public class ReporteMedicoDTO {
 	private String nombreMedico;
 	private String especialidad;
@@ -13,6 +12,30 @@ public class ReporteMedicoDTO {
 	public ReporteMedicoDTO(String nombreMedico,String especialidad,int cantidadTurnos){
 		this.nombreMedico = nombreMedico;
 		this.especialidad = especialidad;
+		this.cantidadTurnos = cantidadTurnos;
+	}
+
+	public String getNombreMedico() {
+		return nombreMedico;
+	}
+
+	public void setNombreMedico(String nombreMedico) {
+		this.nombreMedico = nombreMedico;
+	}
+
+	public String getEspecialidad() {
+		return especialidad;
+	}
+
+	public void setEspecialidad(String especialidad) {
+		this.especialidad = especialidad;
+	}
+
+	public int getCantidadTurnos() {
+		return cantidadTurnos;
+	}
+
+	public void setCantidadTurnos(int cantidadTurnos) {
 		this.cantidadTurnos = cantidadTurnos;
 	}
 }
