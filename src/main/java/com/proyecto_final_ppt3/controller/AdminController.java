e2db3fa2a773702feb128c2971a745e023cc51c0
Tomi-Gomez <tomasngomez1@gmail.com>
2025-10-24 00:19:08 -0300

    feature: agrego endpoint reporteMedico

--- src/main/java/com/proyecto_final_ppt3/controller/AdminController.java
+++ src/main/java/com/proyecto_final_ppt3/controller/AdminController.java
@@ -7,18 +7,23 @@ import com.proyecto_final_ppt3.controller.request.MedicoRequest;
 import com.proyecto_final_ppt3.controller.request.UsuarioRequest;
 import com.proyecto_final_ppt3.controller.response.MedicoResponse;
 import com.proyecto_final_ppt3.service.AdministrativoService;
+import com.proyecto_final_ppt3.service.ReportService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @RestController
 @RequestMapping("/")
 public class AdminController {
 
     private AdministrativoService administrativo;
+
+    private ReportService reportService;
 
     @PostMapping("/insertarAdmin")
     public Administrativo insertarAdmin(@RequestBody  AdministrativoRequest administrativoRequest) {
@@ -30,5 +35,16 @@ public class AdminController {
         return administrativo.insertarMedico(medicoRequest);
     }
 
+    @GetMapping("/reporteMedico")
+    public ResponseEntity<byte[]> generarReporteTurnosxMedico(
+            @RequestParam("inicio") String fechaInicio,
+            @RequestParam("fin") String fechaFin) {
 
+        byte[] pdf = reportService.generarReporteTurnosxMedico(fechaInicio, fechaFin);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=reporte_medicos.pdf")
+                .contentType(MediaType.APPLICATION_PDF)
+                .body(pdf);
+    }
 }
